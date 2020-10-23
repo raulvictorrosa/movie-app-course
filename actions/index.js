5d992d9b9e2e8460f3cae4f10e301945ab5a1908
Raul Rosa <raulvictorrosa@gmail.com>
2020-10-23 09:13:56 -0300

    feat: actions

--- actions/index.js
+++ actions/index.js
@@ -1,17 +1,16 @@
+import axios from "axios";
 
-import axios from 'axios'
+const BASE_URL = "http://localhost:3000";
 
-const BASE_URL = 'http://localhost:3000'
-
-const MOVIE_DATA = []
+const MOVIE_DATA = [];
 
 const CATEGORY_DATA = [
-  {id: 'c-0', name: 'all'},
-  {id: 'c-1', name: 'drama'},
-  {id: 'c-2', name: 'action'},
-  {id: 'c-3', name: 'adventure'},
-  {id: 'c-4', name: 'historical'},
-]
+  { id: "c-0", name: "all" },
+  { id: "c-1", name: "drama" },
+  { id: "c-2", name: "action" },
+  { id: "c-3", name: "adventure" },
+  { id: "c-4", name: "historical" },
+];
 
 // 1. getCategories function
 // 2. get categories in index page
@@ -21,57 +20,37 @@ const CATEGORY_DATA = [
 export const getCategories = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(CATEGORY_DATA)
+      resolve(CATEGORY_DATA);
       // reject('Cannot fetch data!')
-    }, 50)
-  })
-}
+    }, 50);
+  });
+};
 
 export const getMovies = () => {
-  return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
-}
+  return axios.get(`${BASE_URL}/api/v1/movies`).then((res) => res.data);
+};
 
 export const createMovie = (movie) => {
-  movie.id = Math.random().toString(36).substr(2, 5)
-  return axios.post(`${BASE_URL}/api/v1/movies`, movie).then(res => res.data)
-}
+  movie.id = Math.random().toString(36).substr(2, 5);
+  return axios.post(`${BASE_URL}/api/v1/movies`, movie).then((res) => res.data);
+};
 
 export const getMovieById = (id) => {
-  return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
-}
+  return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then((res) => res.data);
+};
 
 export const updateMovie = (movie) => {
-  return axios.patch(`${BASE_URL}/api/v1/movies/${movie.id}`, movie)
-    .then(res => res.data)
-}
+  return axios
+    .patch(`${BASE_URL}/api/v1/movies/${movie.id}`, movie)
+    .then((res) => res.data);
+};
 
 export const deleteMovie = (id) => {
-  return axios.delete(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
-}
-
-
+  return axios
+    .delete(`${BASE_URL}/api/v1/movies/${id}`)
+    .then((res) => res.data);
+};
 
 export const getPosts = () => {
-  return axios.get(`${BASE_URL}/api/v1/posts`).then(res => res.data)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return axios.get(`${BASE_URL}/api/v1/posts`).then((res) => res.data);
+};
